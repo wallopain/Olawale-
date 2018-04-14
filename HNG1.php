55c2a73b3b97e0d86671503266568e34ac016f91
wallopain <36283539+wallopain@users.noreply.github.com>
2018-04-14 12:21:42 +0100

    Add files via upload

---
+++ HNG1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Wale</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="HNG.css" />
+</head>
+<body>
+    
+    <div class="wrap"><img src="back.jpeg"></div>
+    <div class="time">Welcome to HNG <br>
+        <?php
+        echo "The time is " . date("h:i:sa");
+        ?> 
+    </div>
+
+
+</body>
+</html>
